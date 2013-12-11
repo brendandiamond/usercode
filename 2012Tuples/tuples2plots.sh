49e9439b224522a396a3008669ecde44c52d0c22
brendandiamond <brendandiamond@gmail.com>
2013-12-11 12:24:55 -0500

    corrected class def for script

--- 2012Tuples/tuples2plots.sh
+++ 2012Tuples/tuples2plots.sh
@@ -10,6 +10,7 @@ do
   mkdir -p STDOUT
   sed 's:INPUTMCTUPLE:INPUTMCTUPLE/'"$filename"':' <ThreePhotonPlotsv3.cpp >ThreePhotonPlotsv3TEMP.cpp
   sed 's:OUTPUTPLOTS:OUTPUTPLOTS/'"$filename"'_plots:' <ThreePhotonPlotsv3TEMP.cpp >ThreePhotonPlotsv3TEMP.cpp
+  sed 's:ThreePhotonPlotsv3:ThreePhotonPlotsv3TEMP:' <ThreePhotonPlotsv3TEMP.cpp >ThreePhotonPlotsv3TEMP.cpp
   outputfile="STDOUT/"$filename"_stdout.txt"
   root -q -l ThreePhotonPlotsv3TEMP.cpp++ > $outputfile
 # testing line below
