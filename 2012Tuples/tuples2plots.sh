98ff15f4804b3f0764275177ca6655c9658ae83e
brendandiamond <brendandiamond@gmail.com>
2013-12-11 12:10:22 -0500

    mkdirs

--- 2012Tuples/tuples2plots.sh
+++ 2012Tuples/tuples2plots.sh
@@ -7,9 +7,10 @@ do
   filename=${filename%.*}
   echo "Processing $filename file..."
   cd ~
+  mkdir -p OUTPUTPLOTS
+  mkdir -p STDOUT
   sed 's:INPUTMCTUPLE:INPUTMCTUPLE/'"$filename"':' <ThreePhotonPlotsv3.cpp >ThreePhotonPlotsv3TEMP.cpp
   sed 's:OUTPUTPLOTS:OUTPUTPLOTS/'"$filename"'_plots:' <ThreePhotonPlotsv3TEMP.cpp >ThreePhotonPlotsv3TEMP.cpp
-  mkdir -p STDOUT
   outputfile="STDOUT/"$filename"_stdout.txt"
   root -q -l ThreePhotonPlotsv3TEMP.cpp++ > $outputfile
 # testing line below
